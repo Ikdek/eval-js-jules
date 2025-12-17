14f6b3355ab50e900db3812738cceba9ec3edee1
Ikdek <jules.pauchet1g1@gmail.com>
2025-12-17 17:12:51 +0100

    Feat : adding style and content to the details items page

--- Ikmdb/src/components/Card.jsx
+++ Ikmdb/src/components/Card.jsx
@@ -1,10 +1,18 @@
 import "./Card.css"
-
-export default function Card({ title, image }) {
+import { useNavigate } from "react-router"
+import { Star } from "lucide-react"
+import { useState } from "react"
+export default function Card({ title, image, id, rating }) {
+    const navigate = useNavigate()
+    const [isFavorite, setIsFavorite] = useState(false)
     return (
         <div className="card">
-            <h2 className="card-title">{title}</h2>
+            <h2 className="card-title">{title} - {rating} <Star size={20} color="yellow" /></h2>
             <img className="card-img" src={image} />
+            <p>
+                <input className="card-button" type="button" value="Détails" onClick={() => navigate(`/items/${id}`)} />
+                <input className="card-button-favoris" type="button" value={isFavorite ? "Supprimer des favoris" : "Ajouter aux favoris"} onClick={() => setIsFavorite(!isFavorite)} />
+            </p>
         </div>
     )
 }
--- Ikmdb/src/pages/Items/Items.jsx
+++ Ikmdb/src/pages/Items/Items.jsx
@@ -1,10 +1,13 @@
 import { useEffect } from "react"
 import { useState } from "react"
 import { useParams } from "react-router"
-import { Star } from "lucide-react"
+import { useNavigate } from "react-router"
+import { ArrowLeft, Star } from "lucide-react"
+import "./Items.css"
 export default function Items() {
     const id = useParams().id
     const [item, setItem] = useState([])
+    const navigate = useNavigate()
     useEffect(() => {
         fetch(`https://api.rawg.io/api/games/${id}?key=6e05fe27807e4751ad135c397f7e333a&`, {
             method: 'GET',
@@ -19,9 +22,15 @@ export default function Items() {
             .catch(err => console.error("Erreur API:", err))
     }, [id])
     return (
-        <div>
-            <h1>{item.name} {item.rating}<Star size={40} color="yellow" /></h1>
-            <p>{item.description}</p>
+        <div className="item-container">
+            <button onClick={() => navigate(-1)}><ArrowLeft size={40} color="black" /></button>
+            <h1 className="item-title">{item.name} {item.rating}<Star size={40} color="yellow" /></h1>
+            <img className="item-img" src={item.background_image} />
+            <p className="item-description">{item.description}</p>
+            <p className="item-reviews">Nombre de critiques : {item.reviews_count}</p>
+            <p className="item-rating">Note moyenne : {item.rating} <Star size={20} color="yellow" /></p>
+            <p className="item-playtime">Nombre d'heures de jeu moyen : {item.playtime}</p>
+
         </div>
     )
 }
